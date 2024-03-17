3422c3e137a412e3b956137055d63cd72712068c
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-18 00:06:15 +0500

    Add files via upload

---
+++ Q22.js
@@ -0,0 +1,7 @@
+"use strict";
+// lets make an array
+let errorArray = ["Mango", "Banana", "Orange", "Watermelon"];
+//producing an error by accesssing an invalid index of an array
+//console.log(errorArray[5]);
+//removing an error
+console.log(errorArray[2]);
---
+++ Q22.ts
@@ -0,0 +1,6 @@
+// lets make an array
+let errorArray:string[]=["Mango", "Banana","Orange","Watermelon"];
+//producing an error by accesssing an invalid index of an array
+//console.log(errorArray[5]);
+//removing an error
+console.log(errorArray[2]);
